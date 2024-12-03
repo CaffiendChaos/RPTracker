22f66ee7861d43162cc842465e4c3d907aa0a5eb
Chaos <cmrounds422@gmail.com>
2024-12-03 12:18:46 -0500

    Update settings.js

--- source/js/settings.js
+++ source/js/settings.js
@@ -2,5 +2,5 @@ const deployID = 'AKfycbxh_aY9YsIZNeX0kVPXu_37uhfFNrJlqPrtjbpAbpdD9yp6aeUOMhYNwH
 const sheetID = `13ylRx1wxQ0wgBV5gABvfG3vYfq-n3NvEdyqxzcF7DpI`;
 const successMessage = `<p class="fullWidth">Submission successful!</p>
 <button onclick="location.reload();" type="button" class="fullWidth submit">Back to form</button>`;
-const threadTags = ["vital", "priority", "rapidfire", "romantic", "family", "friends", "coworkers"];
+const threadTags = ["plot", "comms", "rapidfire", "group", "au"];
 const chartColors = ['#cf86a8', '#84a9d1', '#af88ba', '#85b8b5', '#b6aa79', '#c9986e', '#84ad85', '#ba7878'];
